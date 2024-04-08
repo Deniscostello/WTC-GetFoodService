6a90145457c554a46749055d1d32e930c44b63c9
Denis Costello <G00396309@atu.ie>
2024-04-08 11:59:19 +0100

    Added food data

---
+++ src/main/java/ie/atu/wtcgetfoodservice/FoodService.java
@@ -0,0 +1,4 @@
+package ie.atu.wtcgetfoodservice;
+
+public class FoodService {
+}
--- src/main/java/ie/atu/wtcgetfoodservice/WtcGetFoodServiceApplication.java
+++ src/main/java/ie/atu/wtcgetfoodservice/WtcGetFoodServiceApplication.java
@@ -2,8 +2,10 @@ package ie.atu.wtcgetfoodservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class WtcGetFoodServiceApplication {
 
     public static void main(String[] args) {
