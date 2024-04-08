d85976ec994fc7ba0df74437707957f3e3bf06c1
Denis Costello <G00396309@atu.ie>
2024-04-08 12:01:04 +0100

    Added recipe client

--- src/main/java/ie/atu/wtcgetfoodservice/RecipeClient.java
+++ src/main/java/ie/atu/wtcgetfoodservice/RecipeClient.java
@@ -1,4 +1,12 @@
 package ie.atu.wtcgetfoodservice;
 
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
+
+@FeignClient(name="recipe-service", url = "http://localhost:8084")
 public interface RecipeClient {
+    @PostMapping("/findRecipes")
+    List<RecipeData> foodDetails(List<String> usersFood);
 }
