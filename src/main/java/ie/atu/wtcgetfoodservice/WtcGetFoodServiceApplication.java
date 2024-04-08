5a8abfe50841164d201634bb548a8a32675f5d32
Denis Costello <G00396309@atu.ie>
2024-04-08 11:48:17 +0100

    First commit

---
+++ src/main/java/ie/atu/wtcgetfoodservice/WtcGetFoodServiceApplication.java
@@ -0,0 +1,13 @@
+package ie.atu.wtcgetfoodservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WtcGetFoodServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WtcGetFoodServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ie/atu/wtcgetfoodservice/WtcGetFoodServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ie.atu.wtcgetfoodservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WtcGetFoodServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
